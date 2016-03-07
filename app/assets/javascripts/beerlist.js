d952ae164675c4447314a90c6f2b90541e886606
Eetu Sepp„l„ <eetu.seppala@helsinki.fi>
2016-03-07 18:20:03 +0200

    muokkaus

--- app/assets/javascripts/beerlist.js
+++ app/assets/javascripts/beerlist.js
@@ -1,4 +1,19 @@
+var BEERS = {};
+
+BEERS.show = function(){
+    var beer_list = [];
+
+    $.each(BEERS.list, function (index, beer) {
+        beer_list.push('<li>' + beer['name'] + '</li>')
+    });
+
+    $("#beers").html('<ul>'+ beer_list.join('') + '</ul>');
+};
+
 $(document).ready(function () {
-    $('#beers').html("hello from javascript");
-    console.log("hello console!");
+
+    $.getJSON('beers.json', function (beers) {
+        BEERS.list = beers;
+        BEERS.show();
+     });
 });
